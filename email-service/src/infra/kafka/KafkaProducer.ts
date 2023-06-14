f327ac5b652300b47cd7c4af9ce4dd85d60be79c
seixeiro <nathan.seixeiro@gmail.com>
2023-06-14 18:56:21 -0300

    create producer kafka class

---
+++ email-service/src/infra/kafka/KafkaProducer.ts
@@ -0,0 +1,26 @@
+import KafkaProducerInterface from "../../interfaces/kafka/kafkaConsumer-interface";
+import { Producer, ProduceRequest } from "kafka-node";
+
+export class KafkaProducer implements KafkaProducerInterface{
+    private producer: Producer
+    constructor(private kafkaClient: any) {
+        this.producer = new Producer(kafkaClient);
+      }
+    
+    send(topic: string, msg: string): void {
+       const ṕayloads: ProduceRequest[] = [
+        {
+            topic,
+            messages: [msg],
+        }
+       ]
+
+       this.producer.send(ṕayloads, (error: any, data: any) => {
+        if (error) {
+            console.error('Error was sent message for kafka:', error);
+        } else{
+            console.log('Message sent for kafka', data);
+        }
+       })
+    }
+}
